fe11595d31add1b060c40a38af2293187bd88a80
TolimanStaR <toliman.st4r@gmail.com>
2021-01-27 21:07:36 +0300

    Denchik local changes

--- Algorithms_TSP_ILS/GCycle.h
+++ Algorithms_TSP_ILS/GCycle.h
@@ -1,145 +1,159 @@
 #ifndef  GCYCLE_H
 #define GCycle_H
+
 #include "Point.h"
 #include <vector>
 #include <fstream>
 #include <string>
 #include<random>
+
 using namespace std;
 
 class GCycle {
-	private:
-		vector<int> cycle;
-		vector<Point> points;
-		double** costMatrix;
-		int n;
+private:
+    vector<int> cycle;
+    vector<Point> points;
+    double **costMatrix;
+    int n;
 
-	public:
+public:
 
-		GCycle(string& ifname) {
-			ifstream myfile(ifname);
-			myfile >> this->n;
-			this->points.resize(this->n);
-			int id, x, y;
-			for (int i = 0; i < this->n; i++) {
-				myfile >> id >> x >> y;
-				points[i].setID(id);
-				points[i].setLoc(x, y);
-			}
-			myfile.close();
+    void readDataStdin(string &ifname) {
+        int n1;
+        cin >> n1;
+        this->n = n1;
+        this->points.resize(this->n);
+        int id, x, y;
+        for (int i = 0; i < this->n; i++) {
+            cin >> id >> x >> y;
+            this->points[i].setID(id);
+            this->points[i].setLoc(x, y);
+        }
+    }
 
-			costMatrix = new double* [this->n];
-			for (int i = 0; i < this->n; i++)
-				costMatrix[i] = new double[n];
+    explicit GCycle(string &ifname) {
+        ifstream myfile(ifname);
+        int n1;
+        myfile >> n1;
+        this->n = n1;
+        this->n = 0;
+        cin >> this->n;
+        this->points.resize(this->n);
+        int id, x, y;
+        for (int i = 0; i < this->n; i++) {
+            cin >> id >> x >> y;
+            this->points[i].setID(id);
+            this->points[i].setLoc(x, y);
+        }
+        myfile.close();
 
-			for (int i = 0; i < this->n; i++)
-				for (int j = 0; j < this->n; j++)
-					costMatrix[i][j] = points[i].EuclideanDistance(points[j]);
+        this->costMatrix = new double *[this->n];
+        for (int i = 0; i < this->n; i++)
+            this->costMatrix[i] = new double[this->n];
 
-			this->cycle.resize(this->n);
-		}
+        for (int i = 0; i < this->n; i++)
+            for (int j = 0; j < this->n; j++)
+                this->costMatrix[i][j] = this->points[i].EuclideanDistance(this->points[j]);
 
-		~GCycle() {
-			for (int i = 0; i < this->n; i++)
-				 delete[] costMatrix[i];
-			delete[] costMatrix;
-		}
+        this->cycle.resize(this->n);
+    }
 
-		bool isCorrect() {
-			vector<int> passed(this->n);
-			for (int i = 0; i < this->n; i++)
-				if (passed[cycle[i]] == 1)
-					return 0;
-				else
-					passed[cycle[i]]++;
-			return 1;
-		}
- 
-		double getCost() {
-			double result = 0;
-			for (int i = 0; i < this->n; i++)
-				result += costMatrix[cycle[i]][cycle[(i + 1)%this->n]];
-			return result;
-		}
+    ~GCycle() {
+        for (int i = 0; i < this->n; i++)
+            delete[] this->costMatrix[i];
+        delete[] this->costMatrix;
+    }
 
-		string getCycle() {
-			string strCycle = "";
-			for (int i = 0; i < this->n; i++)
-				strCycle += to_string(points[cycle[i]].getID()) + "\n";
-			return strCycle;
-		}
+    bool isCorrect() {
+        vector<int> passed(this->n);
+        for (int i = 0; i < this->n; i++)
+            if (passed[this->cycle[i]] == 1)
+                return false;
+            else
+                passed[this->cycle[i]]++;
+        return true;
+    }
 
-		void generatePath() { //ячейки i==j в costMatrix тоже надо проинициализировать
-			random_device seed;
-			cycle[0] = seed() % this->n;
-			points[cycle[0]].pass();
+    double getCost() {
+        double result = 0;
+        for (int i = 0; i < this->n; i++)
+            result += costMatrix[cycle[i]][cycle[(i + 1) % this->n]];
+        return result;
+    }
 
-			for (int i = 0; i < this->n - 1; i++) {
-				double minCost = -1;
-				int bestPoint = -1;
-				for (int j = 0; j < this->n; j++) {
-					if (i == j || points[j].isPassed())
-						continue;
-					double currentCost = costMatrix[i][j];
-					if (minCost == -1) {
-						minCost = currentCost;
-						bestPoint = j;
-					}
-					else if (currentCost < minCost) {
-						minCost = currentCost;
-						bestPoint = j;
-					}
-				}
-				cycle[i + 1] = bestPoint;
-				points[bestPoint].pass();
-			}
-		}
+    string getCycle() {
+        string strCycle;
+        for (int i = 0; i < this->n; i++)
+            strCycle += to_string(this->points[this->cycle[i]].getID()) + "\n";
+        return strCycle;
+    }
 
-		void opt2() { //применяем первое попавшееся улучшение
-			for (int i = 0; i < this->n - 1; i++)
-				for (int j = i + 2; j < this->n; j++) {
-					if (i == 0 && j == this->n - 1)
-						continue;
-					double currentCost = costMatrix[cycle[i]][cycle[i + 1]]
-						+ costMatrix[cycle[j]][cycle[(j + 1) % this->n]];
-					double potentialCost = costMatrix[cycle[i]][cycle[j]]
-						+ costMatrix[cycle[i + 1]][cycle[(j + 1) % this->n]];
-					if (potentialCost < currentCost) { 
-						int tmp = cycle[j];
-						cycle[j] = cycle[i + 1];
-						cycle[i + 1] = tmp;
-						i+=2;
-						j--;
-						while (i<j) { // возможно не учтен случай с нулем
-							int tmp = cycle[j];
-							cycle[j] = cycle[i];
-							cycle[i] = tmp;
-							i++;
-							j--;
-						}
-						return;
-					}
-				}
-		}
+    void generatePath() { //пїЅпїЅпїЅпїЅпїЅпїЅ i==j пїЅ costMatrix пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+        random_device seed;
+        this->cycle[0] = seed() % this->n;
+        this->points[this->cycle[0]].pass();
 
-		void opt4() {
+        for (int i = 0; i < this->n - 1; i++) {
+            double minCost = -1;
+            int bestPoint = -1;
+            for (int j = 0; j < this->n; j++) {
+                if (i == j || this->points[j].isPassed())
+                    continue;
+                double currentCost = this->costMatrix[i][j];
+                if (minCost == -1 || currentCost < minCost) {
+                    minCost = currentCost;
+                    bestPoint = j;
+                }
+            }
+            this->cycle[i + 1] = bestPoint;
+            this->points[bestPoint].pass();
+        }
+    }
 
-		}
+    void opt2() { //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+        for (int i = 0; i < this->n - 1; i++)
+            for (int j = i + 2; j < this->n; j++) {
+                if (i == 0 && j == this->n - 1)
+                    continue;
+                double currentCost = this->costMatrix[this->cycle[i]][this->cycle[i + 1]]
+                                     + this->costMatrix[this->cycle[j]][this->cycle[(j + 1) % this->n]];
+                double potentialCost = this->costMatrix[this->cycle[i]][this->cycle[j]]
+                                       + this->costMatrix[this->cycle[i + 1]][this->cycle[(j + 1) % this->n]];
+                if (potentialCost < currentCost) {
+                    int tmp = this->cycle[j];
+                    this->cycle[j] = this->cycle[i + 1];
+                    this->cycle[i + 1] = tmp;
+                    i += 2;
+                    j--;
+                    while (i < j) { // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ
+                        tmp = this->cycle[j];
+                        this->cycle[j] = this->cycle[i];
+                        this->cycle[i] = tmp;
+                        i++;
+                        j--;
+                    }
+                    return;
+                }
+            }
+    }
 
-		void ILS() {
-			double previousCost = this->getCost();
-			while (1) {
-				this->opt2();
-				double currentCost = this->getCost();
-				if (previousCost != currentCost) {
-					cout << currentCost << endl;
-					previousCost = currentCost;
-				}
-				else
-					break;
-			}
-			//дальше работаем с opt4
-		}
+    void opt4() {
+
+    }
+
+    void ILS() {
+        double previousCost = this->getCost();
+        while (true) {
+            this->opt2();
+            double currentCost = this->getCost();
+            if (previousCost != currentCost) {
+                cout << currentCost << endl;
+                previousCost = currentCost;
+            } else
+                break;
+        }
+        //пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ opt4
+    }
 };
 
 #endif // ! GCYCLE_H
--- Algorithms_TSP_ILS/main.cpp
+++ Algorithms_TSP_ILS/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
 #include "GCycle.h"
+
 using namespace std;
 
 int main() {
-	string my_file = "cases\\case1.txt";
-	GCycle solution(my_file);
-	solution.generatePath();
-	cout << solution.getCost()<<endl;
-	solution.ILS();
-	cout << "Is Gcycle correct?  "<<solution.isCorrect() << endl;
-	cout << endl << solution.getCycle();
+    string my_file = "cases\\case1.txt";
+    GCycle solution(my_file);
+    solution.generatePath();
+    cout << solution.getCost() << endl;
+    solution.ILS();
+    cout << "Is Gcycle correct?  " << solution.isCorrect() << endl;
+    cout << endl << solution.getCycle();
 }
